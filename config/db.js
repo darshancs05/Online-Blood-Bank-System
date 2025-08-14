9e76c2d27b42332e6d634231e78834881530751d
darshancs05 <darshanshanbhag05@gmail.com>
2025-08-14 09:00:38 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+
+// config....
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
+        .white
+    );
+  } catch (error) {
+    console.log(`Mongodb Database Error ${error}`.bgRed.white);
+  }
+};
+
+module.exports = connectDB;
